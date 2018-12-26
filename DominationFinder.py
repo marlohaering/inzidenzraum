aa077f80dcb369afca6bc41b7018399e855facec
Marlo Haering <haering@informatik.uni-hamburg.de>
2018-12-26 16:08:29 +0100

    find domination

--- DominationFinder.py
+++ DominationFinder.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from typing import Set
 import itertools
-from Inzidenzraumcreator import generate_inzidenzraum_with_lines
+from Inzidenzraumcreator import generate_inzidenzraum_with_lines, generate_inzidenzraum_with_points
+from tqdm import tqdm
+from scipy.special import binom
 
 
 def get_points_and_lines(i: pd.DataFrame) -> Set:
@@ -9,6 +11,12 @@ def get_points_and_lines(i: pd.DataFrame) -> Set:
     full_domination.update(i.index.tolist()[:-1])
     full_domination.update(i.columns.tolist()[:-1])
     return full_domination
+
+def get_number_combinations(number_elements, lower_bound, max_bound):
+    combinations = 0
+    for i in range(lower_bound, max_bound+1):
+        combinations += binom(number_elements, i)
+    return combinations
 
 
 def find_domination(i: pd.DataFrame):
@@ -18,10 +26,12 @@ def find_domination(i: pd.DataFrame):
     print(i)
     print(f'bounds: ({lower_bound}, {max_bound})')
     for x in range(lower_bound, max_bound+1):
-        for candidate in itertools.combinations(full_domination, x):
+        iterator = tqdm(itertools.combinations(full_domination, x), total=binom(len(full_domination), x))
+        for candidate in iterator:
             candidate_set = set(candidate)
             if check_domination(i, candidate_set):
                 print(f'found one minimal dominating set: {candidate_set}')
+                iterator.close()
                 return candidate_set
 
 
@@ -45,7 +55,7 @@ def check_domination(i: pd.DataFrame, S: Set) -> bool:
 
 
 if __name__ == "__main__":
-    i = generate_inzidenzraum_with_lines(12)
+    i = generate_inzidenzraum_with_points(6)
     # i.to_pickle('inzidenzraum.pkl')
     # i = pd.read_pickle('inzidenzraum.pkl')
     find_domination(i)
