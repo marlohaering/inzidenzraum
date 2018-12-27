0de4cef84754d3cf3dce1affc7307139bb9bb098
Marlo Haering <haering@informatik.uni-hamburg.de>
2018-12-27 13:20:52 +0100

    disabled domination calculatoin for web server

--- WebServer.py
+++ WebServer.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 @app.route('/points/<int:points>')
 def points(points):
     df = generate_inzidenzraum_with_points(points)
-    domination = find_domination(df)
+    # domination = find_domination(df)
+    domination = 'no domination'
     return render_template("analysis.html", data=df.to_html(), domination=domination)
 
 @app.route('/lines/<int:lines>')
 def lines(lines):
     df = generate_inzidenzraum_with_lines(lines)
-    domination = find_domination(df)
+    # domination = find_domination(df)
+    domination = 'no domination'
     return render_template("analysis.html", data=df.to_html(), domination=domination)
 
 if __name__ == "__main__":
